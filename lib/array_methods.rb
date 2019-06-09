958f09e492146add8e4da352ecacbc2df26e4d45
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-09 18:35:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -25,13 +25,6 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  counter = 0
-  answer = 0
-  while array[counter] do
-    if array[counter] < answer
-      answer = array[counter]
-    end
-      counter +=1
-  end
-  answer
+  array.sort
+  array[0]
 end
