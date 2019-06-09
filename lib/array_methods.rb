14200b2309e5c9b901c6443443d84089f532d9f5
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-09 16:58:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while counter < array.length do |index|
+    if array[index] == value_to_find
+      puts "true"
+    end
+    counter += 1
+  end
+
 end
 
 def find_max_value(array)
